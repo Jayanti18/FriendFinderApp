a4fcf6831b6d07e331cd349b16004c1afd3bb1ee
jayanti18 <jayantirajesh@yahoo.com>
2018-09-29 22:05:41 -0400

    submitting the project friend finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,7 @@
 var friends = [ {
 	"name" :"Ahmed",
-	"photo" :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	// "photo" :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	"photo" :"https://www.google.com/search?q=awesome+pic&rlz=1C5CHFA_enUS790US790&tbm=isch&source=iu&ictx=1&fir=kOf1_BhDg7v_4M%253A%252CF5hoi26pLYY9PM%252C_&usg=AI4_-kRFlMDOUXyb9m6LtCZHxwZ2us3p0g&sa=X&ved=2ahUKEwiTkpnDz-HdAhVEtFMKHaJJD0MQ9QEwCnoECAUQGA#imgrc=kOf1_BhDg7v_4M:",
 	"scores" :[
 		5,
 		1,
